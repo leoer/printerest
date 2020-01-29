b056138140f2e5bbb0483f5713fadadf3bd20644
Finn Böger <finnboeger@googlemail.com>
2020-01-29 14:18:49 +0100

    created basic template and display all images on it

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
 
@@ -14,11 +14,20 @@ def favicon():
 	return send_file("favicon.ico")
 
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def main():
+	# get all images
+	images = dict()
+	for subdirectory in os.listdir(os.path.join("static","images")):
+		# list all files in the subdirectory and add them to the dictionary
+		images[subdirectory] = [x for x in os.listdir(os.path.join("static", "images", subdirectory))
+								# check if filename is an acceptable image format
+								# Fails if files without a "." are present in the directory TODO
+								if x.rsplit(".",1)[1].lower() in ["jpg", "jpeg", "png", "gif"]]
+
+	return render_template("index.html", images=images)
 
 
 if __name__ == "__main__":
 	# Start Server
-	app.run(host='0.0.0.0', port=PORT, threaded=True)
+	app.run(host='0.0.0.0', port=PORT, threaded=True, debug=True)
 	pass
